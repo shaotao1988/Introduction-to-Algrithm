844519c4920a25a8f2b10fa8dfef7fb753e11b9b
shaotao1988 <563725203@qq.com>
2018-01-31 00:36:00 -0500

    add heap sort

--- AlSort.py
+++ AlSort.py
@@ -3,6 +3,49 @@
 
 import sys
 
+class HeapSort(object):
+    def __init__(self, data):
+        self.data = data[:]
+        self.length = len(data)
+        self.heap_size = len(data)
+
+    def left(self, index):
+        return 2*index
+
+    def right(self, index):
+        return 2*index+1
+
+    """
+    Suggest both children rooted at left(index) and right(index) are max heap
+    But data[index] might be smaller than it's children
+    max_heapify maintains the binary tree rooted at index to be max heap
+    """
+    def max_heapify(self, index):
+        l = self.left(index)
+        r = self.right(index)
+        largest = index
+        if l < self.heap_size and self.data[l] > self.data[largest]:
+            largest = l
+        if r < self.heap_size and self.data[r] > self.data[largest]:
+            largest = r
+        # If child is largest, exchange with largest child and continue maintain max heap rooted at child
+        if largest != index:
+            self.data[index], self.data[largest] = self.data[largest], self.data[index]
+            self.max_heapify(largest)
+
+    def make_max_heap(self):
+        # The index bigger than self.length/2 are leaves, ie. max heap with size 1
+        for i in range(self.length//2, -1, -1):
+            self.max_heapify(i)
+
+    def heap_sort(self):
+        self.make_max_heap()
+        for i in range(self.heap_size-1, 0, -1):
+            self.data[i], self.data[0] = self.data[0], self.data[i]
+            self.heap_size -= 1
+            self.max_heapify(0)
+
+
 class AlSort(object):
     def __init__(self, data):
         self.data = data[:]
@@ -55,4 +98,10 @@ if __name__ == "__main__":
     if Sort.data != result:
         print("test MergeSort wrong:", Sort.data)
     print(Sort.data)
+    HR = HeapSort(testdata)
+    HR.heap_sort()
+    if HR.data != result:
+        print("test HeapSort wrong:", HR.data)
+    print(HR.data)
+
 
