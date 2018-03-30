c23506cd4295f1caf4375b34c78b4a0d713bc06a
shaotao1988 <563725203@qq.com>
2018-03-30 16:50:01 -0500

    Greedy algrithm

---
+++ Greedy Algrithm/activity_selector.py
@@ -0,0 +1,45 @@
+# -*- coding = utf-8 -*-
+
+"""
+问题描述
+活动序列a(i)，长度为n，共享一个资源，同时有且只有一个活动可以占有该资源，且活动开始必须持续到结束
+s(i)和f(i)分别是a(i)的开始和结束时间，f(i)已按从小到大排列
+求一个最优方案，从活动a中选出子序列，使可以开展的活动数最大
+"""
+
+"""
+动态规划
+令L(i)是以a(i)结束的最长兼容子序列
+那么L(i)=max L(j)+1, 当s(i) >= f(j), j从0...i-1
+            L(j), 当s(i) < f(j), j从0...i-1
+"""
+def dynamic_activity_selector(s, f, n):
+    l = [1]*n
+    largest = 1
+    for i in range(1, n):
+        for j in range(0, i):
+            cur = l[j]
+            if s[i] >= f[j]:
+                cur = l[j] + 1
+            l[i] = max(l[i], cur)
+        if largest < l[i]:
+            largest = l[i]
+    #print(l)
+    return largest
+
+"""
+贪婪算法
+每次选取活动时，选结束最早的那个
+"""
+def recursive_activity_selector(s, f, n):
+    # 需要加上a[0]，因为recursive_activity_selector_util找的是a[0]结束后最长的活动个数
+    return recursive_activity_selector_util(s, f, 0, n)+1
+
+# 寻找第k+1到n中，a[k]结束后第一个开始的活动
+def recursive_activity_selector_util(s, f, k, n):
+    if k >= n-1:
+        return 0
+    i = k+1
+    while i <= n-1 and s[i] < f[k]:
+        i += 1
+    return recursive_activity_selector_util(s, f, i, n)+1
---
+++ Greedy Algrithm/test_activity_selector.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from activity_selector import *
+
+class TestActivitySelector(unittest.TestCase):
+    
+    def setUp(self):
+        self.s = [1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12]
+        self.f = [4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16]
+        self.largest = 4
+
+    def tearDown(self):
+        pass
+
+    def test_dynamic_selector(self):
+        self.assertEqual(dynamic_activity_selector(self.s, self.f, len(self.s)), self.largest)
+
+    def test_recursive_activity_selector(self):
+        self.assertEqual(recursive_activity_selector(self.s, self.f, len(self.s)), self.largest)
+
+if __name__ == "__main__":
+    unittest.main()
